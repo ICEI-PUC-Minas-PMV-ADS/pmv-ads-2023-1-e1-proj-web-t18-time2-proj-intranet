be9a8b533f76dd3d21b91afa62af0ca6d1e4186e
hashtagluan <128554958+hashtagluan@users.noreply.github.com>
2023-05-21 13:49:49 -0300

    atualização home page

---
+++ src/base.js
@@ -0,0 +1,25 @@
+var db = {
+    dados: [
+        {
+            titulo: 'titulo da noticia',
+            descricao: 'Postas de robalo feitas na farinha de trigo. Acompanha arroz e batatas',
+
+        }, {
+            titulo: 'Agua com gás',
+            descricao: 'Agua mineral gaseificada naturalmente da fonte de hidrópolis. Muito saudável',
+
+        }, {
+            titulo: 'Bife a cavalo',
+            descricao: 'Escalopes de filé, grelhados com ovo frito por cima. Arroz e batatas',
+
+        }, {
+            titulo: 'Regrigerante guarana',
+            descricao: 'Guarana da amazonia feito a partir de culturas organicas',
+
+        }, { 
+            titulo: 'Doce de leite',
+            descricao: 'Porçao de doce de leie feito artesanalmente feito em minas gerais',
+        }
+    ]
+}
+
