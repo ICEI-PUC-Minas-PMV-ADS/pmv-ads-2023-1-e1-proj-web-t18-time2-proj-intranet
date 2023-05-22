78c6c48874000a328da9d91f8875ad6330c901f7
hashtagluan <128554958+hashtagluan@users.noreply.github.com>
2023-05-21 22:57:21 -0300

    inclusao da função "ausencias" na home page

---
+++ src/ausencias.js
@@ -0,0 +1,29 @@
+var db = {
+    dados: [
+        {
+            Nome: 'Jose Manoel',
+            Período: '15/01/2024 a 15/02/2024',
+            Motivo: 'Férias',
+
+        }, {
+            Nome: 'Maria Jose',
+            Período: '13/09/2024 a 15/04/2025',
+            Motivo: 'Férias',
+
+        }, {
+            Nome: 'Raimundo Nonato',
+            Período: '15/01/2024 a 15/02/2024',
+            Motivo: 'Férias',
+
+        }, {
+            Nome: 'Stela Artuois',
+            Período: '15/01/2024 a 15/02/2024',
+            Motivo: 'Atestado',
+
+        }, { 
+            Nome: 'Bruno Weine',
+            Período: '15/01/2024 a 15/02/2024',
+            Motivo: 'Viagem',
+        }
+    ]
+}
